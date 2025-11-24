ecdcda073bb6667247f4919a744ad16577141b67
Azucena-Ventimilla <ventimillazucena.64@gmail.com>
2025-11-24 12:54:45 -0600

    Update Board.java

---
+++ src/Board.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+import edu.macalester.graphics.Rectangle;
+
+public class Board {
+    int TOTAL_CELLS = 100;
+    int CELL_SIZE = 10;
+    
+    public Board() {
+        int [] mazeLayout = {0,1,0,1,0,1,0,1,0,1,
+                            1,0,1,0,1,0,1,0,1,0,
+                            0,1,0,1,0,1,0,1,0,1,
+                            1,0,1,0,1,0,1,0,1,0,
+                            0,1,0,1,0,1,0,1,0,1,
+                            1,0,1,0,1,0,1,0,1,0,
+                            0,1,0,1,0,1,0,1,0,1,
+                            1,0,1,0,1,0,1,0,1,0,
+                            0,1,0,1,0,1,0,1,0,1,
+                            1,0,1,0,1,0,1,0,1,0
+        };
+    
+    }
+  
+    public void drawMazeLayout() {
+        // Assigning 0 = white, and 1 = black
+      
+    }
+
+    public void drawRectangle() {
+        
+    }
+}
