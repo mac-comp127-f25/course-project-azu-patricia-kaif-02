cbf46499053742b95c737c9da815aff2f497429a
Patricia Escobar <pescobar@macalester.edu>
2025-12-01 14:47:07 -0600

    updating the board

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,24 @@
 import java.util.ArrayList;
 
+import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
+import edu.macalester.graphics.*;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 
 public class Board {
-    int TOTAL_CELLS = 100;
-    int CELL_SIZE = 10;
+    private static final int TOTAL_CELLS = 100;
+    private static final int CELL_SIZE = 10;
+
+    private int[] mazeLayout;
+
     
     public Board() {
-        int [] mazeLayout = {0,1,0,1,0,1,0,1,0,1,
+        int[] mazeLayout = {0,1,0,1,0,1,0,1,0,1,
                             1,0,1,0,1,0,1,0,1,0,
                             0,1,0,1,0,1,0,1,0,1,
                             1,0,1,0,1,0,1,0,1,0,
@@ -18,15 +29,38 @@ public class Board {
                             0,1,0,1,0,1,0,1,0,1,
                             1,0,1,0,1,0,1,0,1,0
         };
-    
     }
   
-    public void drawMazeLayout() {
-        // Assigning 0 = white, and 1 = black
-      
+    public void drawMazeLayout(CanvasWindow canvas) {
+        int index = 0;
+
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+
+                Rectangle cell = new Rectangle(
+                    col * CELL_SIZE,
+                    row * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE
+                );
+
+                if (mazeLayout[index] == 0) {
+                    cell.setFillColor(Color.WHITE);
+                } else {
+                    cell.setFillColor(Color.BLACK);
+                }
+
+                cell.setFilled(true);
+                canvas.add(cell);
+
+                index++;
+        }
+    }
     }
 
-    public void drawRectangle() {
-        
+   public static void main(String[] args) {
+        CanvasWindow canvas = new CanvasWindow("Maze", CELL_SIZE*TOTAL_CELLS, CELL_SIZE*TOTAL_CELLS);
+        Board board = new Board();
+        board.drawMazeLayout(canvas);      
     }
 }
