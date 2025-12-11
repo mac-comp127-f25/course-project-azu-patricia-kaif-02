d6159cf66a5a0bbba74d755ea6665c18767c2625
Patricia Escobar <pescobar@macalester.edu>
2025-12-11 14:18:22 -0600

    Update King.java

--- src/King.java
+++ src/King.java
@@ -1,10 +1,10 @@
 
 
 /**
- * Represents a King piece, capable of diagonal movement.
+ * Represents a King piece that can move diagonally.
  */
 
-public class King implements Chip {
+public class King implements {
     
     private boolean selected = false;
     private double mouseX;
